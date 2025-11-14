e241292c7475e54a0e2a69d938077f2bc87eddf5
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-11-14 10:51:40 +0100

    Try without LTO on Linux

--- scripts/toolchain/linux/in-container.sh
+++ scripts/toolchain/linux/in-container.sh
@@ -42,13 +42,12 @@ build_llvm() {
   build_dir="build_llvm"
   cmake -S llvm -B "$build_dir" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_AR=$(which llvm-ar) \
-    -DCMAKE_RANLIB=$(which llvm-ranlib) \
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++ \
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
     -DLLVM_BUILD_EXAMPLES=OFF \
     -DLLVM_BUILD_TESTS=OFF \
     -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_ENABLE_LTO=Thin \
     -DLLVM_ENABLE_PROJECTS=mlir \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_INCLUDE_EXAMPLES=OFF \
