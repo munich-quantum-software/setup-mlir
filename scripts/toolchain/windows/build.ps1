69812fe2f7c2187e04e89c1b4f26911446734dc9
burgholzer <burgholzer@me.com>
2025-11-15 11:49:21 -0500

    📄 update license info

--- scripts/toolchain/windows/build.ps1
+++ scripts/toolchain/windows/build.ps1
@@ -1,5 +1,15 @@
-# Copyright (c) 2025 Lukas Burgholzer
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# Copyright (c) 2025 Chair for Design Automation, TUM
 # All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions (the "License"); you
+# may not use this file except in compliance with the License. You may obtain a
+# copy of the License at https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
